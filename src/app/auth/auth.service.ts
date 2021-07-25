e7a43137b3ab3b050006ed7c64779091d955ea35
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-25 21:45:39 +0200

    created save and clear auth data methods to which I used them in login and logout methods of auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -47,6 +47,9 @@ export class AuthService {
           }, expiresInDuration * 1000);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+          this.saveAuthData(token, expirationDate);
           this.router.navigate(['/']);
         }
       });
@@ -57,6 +60,17 @@ export class AuthService {
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
     clearTimeout(this.tokenTimer);
+    this.clearAuthData();
     this.router.navigate(['/']);
+  }
+
+  private saveAuthData(token: string, expirationDate: Date) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expiration");
   }
 }
