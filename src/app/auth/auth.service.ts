639664bcd0c6ed02eba856ba5b4207cda8022a60
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-25 17:42:16 +0200

    created auth-data model

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,11 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-
+  constructor(private http: HttpClient) { }
 
   createUser(email: string, password: string) {
-
+    this.http.post("http://localhost:3000/api/user/signup")
   }
 }
