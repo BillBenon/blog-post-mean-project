48a318a9d270b07b260b44b651a7bb194be94765
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-27 11:35:05 +0200

    fixed an error of infinite spinning while the user have signed up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -35,7 +35,7 @@ export class AuthService {
     const authData: AuthData = { email, password };
     this.http.post("http://localhost:3000/api/user/signup", authData)
       .subscribe(response => {
-        console.log(response);
+        this.router.navigate(['/login']);
       });
   }
 
