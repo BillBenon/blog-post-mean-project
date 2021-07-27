6dd8dbd975d35ab67df90025492ef167517c74d9
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-27 19:40:03 +0200

    userLogin controller worked on; sorry for the last commit, it is createUser controller not middleware

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,8 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
 exports.createUser = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
@@ -17,5 +22,41 @@ exports.createUser = (req, res, next) => {
             message: "Invalid authentication credentials!"
           });
         });
+    });
+}
+
+exports.userLogin = (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Authentication failed!"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Email or password is incorrect!"
+        });
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "noneb9587938*$(gnik$snakpumums)",
+        { expiresIn: '1h'}
+      );
+      res.status(200).json({
+        token,
+        expiresIn: 3600,
+        userId: fetchedUser._id
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Invalid authentication credentials!"
+      });
     });
 }
