b296f8002be437a55aa53d2e7b7746cdb28db3d0
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-27 19:49:19 +0200

    createPost controller done

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -28,29 +28,7 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post("", checkAuth, multer({storage}).single("image"), (req, res, next) => {
-  const url = req.protocol + '://' + req.get("host");
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename,
-    creator: req.userData.userId
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post added successfully!',
-      post: {
-        ...createdPost,
-        id: createdPost._id
-      }
-    });
-  })
-  .catch(error => {
-    res.status(500).json({
-      message: "Creating a post failed!"
-    });
-  });
-});
+router.post("", checkAuth, multer({storage}).single("image"), );
 
 router.put("/:id", checkAuth, multer({storage}).single("image"), (req, res, next) => {
   let imagePath = req.body.imagePath;
