191a8ce4373846bc77d55542bb333e4476d3475f
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-27 19:56:32 +0200

    getPost controller in post.js done

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -77,3 +77,17 @@ exports.getPosts = (req, res, next) => {
     });
   });
 }
+
+exports.getPost = (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Post not found!' });
+    }
+  }).catch(error => {
+    res.status(500).json({
+      message: "Fetching post failed!"
+    });
+  });
+}
