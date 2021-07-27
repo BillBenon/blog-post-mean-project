00c995dc16e34e9b8e250591fc4acbd7953b2930
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-27 19:53:50 +0200

    updatePost controller in post.js file done

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -23,3 +23,30 @@ exports.createPost = (req, res, next) => {
     });
   });
 }
+
+exports.updatePost = (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if (req.file) {
+    const url = req.protocol + "://" + req.get("host");
+    imagePath = url + "/images/" + req.file.filename;
+  }
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content,
+    imagePath,
+    creator: req.userData.userId
+  });
+  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
+    if (result.nModified > 0) {
+      res.status(200).json({ message: "Updated successfully!" });
+    } else {
+      res.status(403).json({ message: "Not authorized!" });
+    }
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: "Couldn't update post!"
+    });
+  });
+}
