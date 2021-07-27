f757673eb68d3db570a3a001fbf6838619bb8b3a
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-27 10:34:59 +0200

    added userData containing email & userId to the request body while creating new post to also include creator field

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try{
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, "noneb9587938*$(gnik$snakpumums)");
+    const decodedToken = jwt.verify(token, "noneb9587938*$(gnik$snakpumums)");
+    req.userData = { email: decodedToken.email, userId: decodedToken.userId };
     next();
   } catch (error) {
     res.status(401).json({ message: "Authentication failed!" });
