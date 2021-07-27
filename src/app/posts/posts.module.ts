c7a423e29f0f1872718b68c5b05ce0f3f84186d4
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-27 20:27:55 +0200

    fixed all the errors, now the app is running well

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,6 +1,9 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
+import { AngularMaterialModule } from "../angular-material.module";
 import { PostCreateComponent } from './post-create/post-create.component';
 import { PostListComponent } from './post-list/post-list.component';
 
@@ -10,7 +13,10 @@ import { PostListComponent } from './post-list/post-list.component';
     PostListComponent
   ],
   imports: [
-    ReactiveFormsModule
+    CommonModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    RouterModule
   ]
 })
 export class PostsModule { }
