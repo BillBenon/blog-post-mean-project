d9bcd202287f8542b33ced70b9bd7c4ebb457a30
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-27 19:50:32 +0200

    added post model to posts.js controller

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const multer = require('multer');
 
-const Post = require('../models/post');
 const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
