deefbbc6c1c6ee85a5922d5fbf376396807485de
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-28 17:49:30 +0200

    configured backend user url in auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,7 +3,10 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { Subject } from "rxjs";
 
+import { environment } from 'src/environments/environment';
 import { AuthData } from "./auth-data.model";
+
+const BACKEND_URL = environment.apiUrl + "/user";
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -33,7 +36,7 @@ export class AuthService {
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email, password };
-    this.http.post("http://localhost:3000/api/user/signup", authData)
+    this.http.post(BACKEND_URL + "/signup", authData)
       .subscribe(() => {
         this.router.navigate(["/"]);
       }, error => {
@@ -43,7 +46,7 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = { email, password };
-    this.http.post<{ token: string, expiresIn: number, userId: string }>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{ token: string, expiresIn: number, userId: string }>(BACKEND_URL + "/login", authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
